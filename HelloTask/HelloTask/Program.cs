489cc17ccc1e804582d65e30529e4ac2806b1c03
thelittlecitizen <thelittlecitizen1@gmail.com>
2020-08-25 10:40:06 +0300

    1.B

--- HelloTask/HelloTask/Program.cs
+++ HelloTask/HelloTask/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Threading;
 
 namespace HelloTask
 {
     
     class Program
     {
+        private static int number;
 
         static void Main(string[] args)
         {
-            DateTime start = DateTime.Now;
-            for (int i= 0; i<1000000; i++)
-                Console.WriteLine($"the num is {i}");
-            Console.WriteLine((DateTime.Now- start).TotalMilliseconds);
+             DateTime start = DateTime.Now;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                number = i;
+                Thread thread = new Thread(() => Console.WriteLine(number));
+                thread.Start();
+            }
+           
+           Console.WriteLine((DateTime.Now- start).TotalMilliseconds);
         }
+        
     }
 }
