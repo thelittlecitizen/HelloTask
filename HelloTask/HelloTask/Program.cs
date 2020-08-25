1bba69e3c1e6e0f569dc90ba618d5d060f904ef1
thelittlecitizen <thelittlecitizen1@gmail.com>
2020-08-25 09:54:30 +0300

    1.A

---
+++ HelloTask/HelloTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloTask
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            for (int i= 0; i<1000000; i++)
+                Console.WriteLine($"the num is {i}");
+            Console.WriteLine((DateTime.Now- start).TotalMilliseconds);
+        }
+    }
+}
